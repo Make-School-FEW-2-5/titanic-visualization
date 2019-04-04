2a5eed7cb0407b3a5e14e2afcfdcc940edbb656c
Severiano Badajoz <sevebadajoz@gmail.com>
2019-04-04 12:51:34 -0700

    Create multiplier const

--- src/Circle.js
+++ src/Circle.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import styled from 'styled-components/macro'
 
+const vwMultiplier = 30;
+
 const Bubble = styled.div`
   border-radius: 50%;
-  width: ${props => props.size * 50 + 'vw'};
-  height: ${props =>  props.size * 50 + 'vw'};
+  width: ${props => props.size * vwMultiplier + 'vw'};
+  height: ${props =>  props.size * vwMultiplier + 'vw'};
   position:  ${props => props.children.type? 'relative': 'absolute'};
   left: ${props => props.children.type? '0': '0vw'};
-  top: ${props => props.children.type? '0':25 - (props.size * 25) + 'vw'};
+  top: ${props => props.children.type? '0':vwMultiplier / 2 - (props.size * vwMultiplier / 2) + 'vw'};
   background-color: ${props => props.color || 'red'};
   text-align: center;
-  line-height:${props => props.size * 50 + 'vw'};
+  line-height:${props => props.size * vwMultiplier + 'vw'};
 `;
 
 
