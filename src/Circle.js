43be03f31804022484f58422bfe5e30c32f11d60
Severiano Badajoz <sevebadajoz@gmail.com>
2019-04-04 10:55:06 -0700

    Add styling to center text and prep for animating

--- src/Circle.js
+++ src/Circle.js
@@ -5,12 +5,19 @@ const Bubble = styled.div`
   border-radius: 50%;
   width: ${props => props.size * 50 + 'vw'};
   height: ${props =>  props.size * 50 + 'vw'};
+  position:  ${props => props.children.type? 'relative': 'absolute'};
+  left: ${props => props.children.type? '0': '0vw'};
+  top: ${props => props.children.type? '0':25 - (props.size * 25) + 'vw'};
   background-color: ${props => props.color || 'red'};
+  text-align: center;
+  line-height:${props => props.size * 50 + 'vw'};
 `;
 
 
 export default class Circle extends Component {
   constructor(props) {
+    console.log(props.children);
+    
     super(props)
   
     this.state = {
@@ -20,7 +27,7 @@ export default class Circle extends Component {
   
   render() {
     return (
-      <Bubble {...this.props}>
+      <Bubble {...this.props} ref={ref => this.bubble = ref}>
         {this.props.children || this.props.number}
       </Bubble>
     )
